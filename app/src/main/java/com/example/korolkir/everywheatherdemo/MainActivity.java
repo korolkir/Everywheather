218cba548d86be8ba7e893f6b06cbe51bb0654b8
korolkir <korolkir@gmail.com>
2016-07-21 14:07:34 +0300

    initial commit

--- app/src/main/java/com/example/korolkir/everywheatherdemo/MainActivity.java
+++ app/src/main/java/com/example/korolkir/everywheatherdemo/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.korolkir.everywheatherdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
+        //dsd
     }
 }
