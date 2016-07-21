ecff02938c2bf9c9ffadc47ec55778bfe2230aeb
korolkir <korolkir@gmail.com>
2016-07-21 17:35:14 +0300

    add ui: current day view and recycler view

--- app/src/main/java/com/example/korolkir/everywheatherdemo/MainActivity.java
+++ app/src/main/java/com/example/korolkir/everywheatherdemo/MainActivity.java
@@ -2,16 +2,23 @@ package com.example.korolkir.everywheatherdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
 import com.crashlytics.android.Crashlytics;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.hello_world) TextView helloWorld;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
-        //dsd
+        ButterKnife.bind(this);
     }
 }
