88078e5cbc5e0db80603697320ad6daf59dd46bc
korolkir <korolkir@gmail.com>
2016-07-22 01:44:54 +0300

    add current day forecast showing

--- app/src/main/java/com/example/korolkir/everywheatherdemo/ForecastPresenter.java
+++ app/src/main/java/com/example/korolkir/everywheatherdemo/ForecastPresenter.java
@@ -1,10 +1,23 @@
 package com.example.korolkir.everywheatherdemo;
 
 /**
- * Created by korolkir on 22.07.16.
+ * Created by korolkir on 21.07.16.
  */
-public interface ForecastPresenter {
+public class ForecastPresenter {
 
-    void applyForecast(Forecast forecast);
+    private MainActivity mView;
+    private ForecastCreator mForecastCreator;
 
+    public ForecastPresenter(MainActivity mView) {
+        this.mView = mView;
+        this.mForecastCreator = new ForecastCreator();
+    }
+
+    public void getForecast() {
+        mForecastCreator.createForecast(this);
+    }
+
+    public void applyForecast(Forecast forecast) {
+        if(forecast != null) mView.showForecast(forecast);
+    }
 }
--- app/src/main/java/com/example/korolkir/everywheatherdemo/MainActivity.java
+++ app/src/main/java/com/example/korolkir/everywheatherdemo/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.korolkir.everywheatherdemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.crashlytics.android.Crashlytics;
 
@@ -12,7 +13,7 @@ import io.fabric.sdk.android.Fabric;
 public class MainActivity extends AppCompatActivity {
 
     //@BindView(R.id.hello_world) TextView helloWorld;
-    private ForecastPresenterImpl mPresenter;
+    private ForecastPresenter mPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +21,24 @@ public class MainActivity extends AppCompatActivity {
         Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        mPresenter = new ForecastPresenterImpl(this);
+        mPresenter = new ForecastPresenter(this);
         mPresenter.getForecast();
     }
 
-    public void showForecast() {
+    public void showForecast(Forecast forecast) {
         Log.i("MainActivity", "Forecast showing!");
+        showCurrentDayForecast(forecast.getWeatherList().get(0));
+    }
+
+    private void showCurrentDayForecast(Weather weather) {
+        TextView currentDayTemp = ButterKnife.findById(this, R.id.current_day_temperature);
+        currentDayTemp.setText(weather.getTemperature()+"º");
+        TextView currentDayDescription = ButterKnife.findById(this, R.id.current_day_description);
+        currentDayDescription.setText(weather.getDescription());
+        TextView currentDaySpeed = ButterKnife.findById(this, R.id.current_day_speed);
+        currentDaySpeed.setText(String.valueOf(weather.getSpeed()) + " mph");
+        TextView currentDayTemperatureRange = ButterKnife.findById(this, R.id.current_day_temperature_range);
+        currentDayTemperatureRange.setText(String.valueOf(weather.getTempMin()) +"º"+ " - " + weather.getTempMax() + "º");
     }
 
 
