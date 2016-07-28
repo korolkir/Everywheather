877bbf99388fa14cfdb34f248bb18f7d0e789082
korolkir <korolkir@gmail.com>
2016-07-28 13:00:41 +0300

    add suggestion using google places api

--- app/src/main/java/com/example/korolkir/everywheatherdemo/MainActivity.java
+++ app/src/main/java/com/example/korolkir/everywheatherdemo/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.korolkir.everywheatherdemo;
 
+import android.app.SearchManager;
+import android.content.ComponentName;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.provider.SearchRecentSuggestions;
+import android.support.annotation.NonNull;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.text.TextPaint;
+import android.text.style.CharacterStyle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -20,17 +30,26 @@ import com.crashlytics.android.Crashlytics;
 import com.example.korolkir.everywheatherdemo.Model.Weather;
 import com.example.korolkir.everywheatherdemo.Presenter.ForecastPresenterImplementor;
 import com.example.korolkir.everywheatherdemo.View.ShowingView;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.data.DataBufferUtils;
+import com.google.android.gms.location.places.AutocompletePredictionBuffer;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceBuffer;
+import com.google.android.gms.location.places.Places;
 import com.squareup.picasso.Picasso;
 import com.yqritc.recyclerviewflexibledivider.HorizontalDividerItemDecoration;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.fabric.sdk.android.Fabric;
 
-public class MainActivity extends AppCompatActivity implements ShowingView {
+public class MainActivity extends AppCompatActivity implements ShowingView,SearchView.OnQueryTextListener {
 
     @BindView(R.id.recycler_view) RecyclerView recyclerView;
     @BindView(R.id.current_day_description) TextView currentDayDescription;
@@ -43,6 +62,7 @@ public class MainActivity extends AppCompatActivity implements ShowingView {
     private ForecastPresenterImplementor mPresenter;
     private List<DailyForecast> mDailyForecastList;
     private WeatherRecyclerViewAdapter mAdapter;
+    //private Suggestion mProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,11 +83,22 @@ public class MainActivity extends AppCompatActivity implements ShowingView {
                         .build());
         mPresenter = new ForecastPresenterImplementor(this);
         mPresenter.getForecast();
+        //mProvider = new Suggestion(this);
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuItem searchItem = menu.findItem(R.id.search);
+
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setOnQueryTextListener(this);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(
+                new ComponentName(this, MainActivity.class)));
+        searchView.setIconifiedByDefault(false);
         return true;
     }
 
@@ -106,11 +137,9 @@ public class MainActivity extends AppCompatActivity implements ShowingView {
 
     @Override
     public void showWeatherList(List<DailyForecast> weatherList) {
-        Log.i("ListInfo", String.valueOf(weatherList.size()));
         mDailyForecastList.clear();
         mDailyForecastList.addAll(weatherList);
         mAdapter.notifyDataSetChanged();
-        Log.i("ItemCount", String.valueOf(mAdapter.getItemCount()));
     }
 
     @Override
@@ -118,4 +147,18 @@ public class MainActivity extends AppCompatActivity implements ShowingView {
         return this;
     }
 
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(final String newText) {
+        //mProvider.getCitiesList(newText);
+        SearchRecentSuggestions suggestions = new SearchRecentSuggestions(this,
+                CitySuggestionProvider.AUTHORITY, CitySuggestionProvider.MODE);
+        suggestions.saveRecentQuery(newText, null);
+        return false;
+    }
 }
